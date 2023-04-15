7b61eb93bc2837f9309c5b8cad91ff357a2f7f08
Adrian Ramzes <adrian@karalus.eu>
2023-04-15 23:14:32 +0200

    dummy commit to check if author is set correctly

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -3,3 +3,7 @@ import { Greeter } from '../src';
 test('My Greeter', () => {
   expect(Greeter('World')).toBe('Hello World');
 });
+
+test('My Greeter', () => {
+  expect(Greeter('Adrian')).toBe('Hello Adrian');
+});
